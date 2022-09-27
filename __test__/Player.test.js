42939260358ede1e80a1993204921ee4ea0137fa
Luis S <Luissalcedo2355@hotmail.com>
2022-09-27 02:27:51 -0700

    player objects and prototypes

--- __test__/Player.test.js
+++ __test__/Player.test.js
@@ -1,4 +1,6 @@
 const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion');
+jest.mock('../lib/Potion')
  
 test('create a player object', () => {
     const player = new Player('Dave');
@@ -7,4 +9,25 @@ test('create a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+
+    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("get player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+
+});
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player ('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
 });
